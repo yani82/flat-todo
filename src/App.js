892fdcb02c26eecb5cc8d9f14ed8d839fa883393
yani82 <yani.iman1@gmail.com>
2021-08-31 00:02:24 -0400

    practicing forms

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react' 
+import TodoListItem from "./components/TodoListItem";
+import Header from "./components/Header";
+
+class App extends React.Component {
+state = {
+  todos: [
+    { id: 1, text: "Do React Labs", completed: false },
+    { id: 2, text: "Watch React Lectures", completed: true },
+  ],
+  newTodo: ""
+};
+
+handleChange = (event) => {
+  this.setState({ newTodo: event.target.value })
+  // console.log("the input has changed");
+};
+
+handleSubmit = (event) => {
+  event.preventDefault(); 
+  console.log("submitting new to-do")
+  const newTodoObject = {id: }
+}
+
+  render() {
+    const todoElements = this.state.todos.map((todo) => (
+      <TodoListItem key={todo.id} todo={todo} />
+    ));
+    return (
+    <div>
+      <Header />
+      <form onSubmit={this.handleSubmit}>
+        <input 
+        onChange={this.handleChange} 
+        value={this.state.newTodo}
+        type="text" 
+        placeholder="Enter new to-do here..."
+        />
+        <button type="submit">Create new to-do</button>
+      </form>
+      {todoElements}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h1>To-do List</h1>
+        </div>
+    );
+}
